d5e8cb56ac889a9b85ff197635f1d97948fa0b87
Ryo Takeuchi <develop@tatsutakein.jp>
2025-05-28 15:22:22 +0900

    fix: update quick_add_quest_dialog to use new use case parameters

--- app/mobile/lib/router/app_router.g.dart
+++ app/mobile/lib/router/app_router.g.dart
@@ -7,86 +7,88 @@ part of 'app_router.dart';
 // **************************************************************************
 
 List<RouteBase> get $appRoutes => [
-  $appShellRouteData,
-  $authRoute,
-  $debugScreenRoute,
-  $dataStoreSettingsScreenRoute,
-  $onboardingRoute,
-];
+      $appShellRouteData,
+      $authRoute,
+      $debugScreenRoute,
+      $dataStoreSettingsScreenRoute,
+      $onboardingRoute,
+    ];
 
 RouteBase get $appShellRouteData => StatefulShellRouteData.$route(
-  factory: $AppShellRouteDataExtension._fromState,
-  branches: [
-    StatefulShellBranchData.$branch(
-      navigatorKey: HomeBranch.$navigatorKey,
-      routes: [
-        GoRouteData.$route(
-          path: '/home',
-          factory: $HomeRouteExtension._fromState,
+      factory: $AppShellRouteDataExtension._fromState,
+      branches: [
+        StatefulShellBranchData.$branch(
+          navigatorKey: HomeBranch.$navigatorKey,
           routes: [
             GoRouteData.$route(
-              path: 'feeds',
-              parentNavigatorKey: FeedListRoute.$parentNavigatorKey,
-              factory: $FeedListRouteExtension._fromState,
+              path: '/home',
+              factory: $HomeRouteExtension._fromState,
               routes: [
                 GoRouteData.$route(
-                  path: ':feedId',
-                  parentNavigatorKey: FeedDetailRoute.$parentNavigatorKey,
-                  factory: $FeedDetailRouteExtension._fromState,
+                  path: 'feeds',
+                  parentNavigatorKey: FeedListRoute.$parentNavigatorKey,
+                  factory: $FeedListRouteExtension._fromState,
+                  routes: [
+                    GoRouteData.$route(
+                      path: ':feedId',
+                      parentNavigatorKey: FeedDetailRoute.$parentNavigatorKey,
+                      factory: $FeedDetailRouteExtension._fromState,
+                    ),
+                  ],
+                ),
+                GoRouteData.$route(
+                  path: 'quickAddQuest',
+                  parentNavigatorKey:
+                      QuickAddQuestDialogRoute.$parentNavigatorKey,
+                  factory: $QuickAddQuestDialogRouteExtension._fromState,
                 ),
               ],
             ),
-            GoRouteData.$route(
-              path: 'quickAddQuest',
-              parentNavigatorKey: QuickAddQuestDialogRoute.$parentNavigatorKey,
-              factory: $QuickAddQuestDialogRouteExtension._fromState,
-            ),
           ],
         ),
-      ],
-    ),
-    StatefulShellBranchData.$branch(
-      navigatorKey: QuestBranch.$navigatorKey,
-      routes: [
-        GoRouteData.$route(
-          path: '/quests',
-          factory: $QuestListRouteExtension._fromState,
+        StatefulShellBranchData.$branch(
+          navigatorKey: QuestBranch.$navigatorKey,
           routes: [
             GoRouteData.$route(
-              path: 'add',
-              factory: $QuestAddRouteExtension._fromState,
-            ),
-            GoRouteData.$route(
-              path: ':questId',
-              factory: $QuestDetailRouteExtension._fromState,
+              path: '/quests',
+              factory: $QuestListRouteExtension._fromState,
+              routes: [
+                GoRouteData.$route(
+                  path: 'add',
+                  factory: $QuestAddRouteExtension._fromState,
+                ),
+                GoRouteData.$route(
+                  path: ':questId',
+                  factory: $QuestDetailRouteExtension._fromState,
+                ),
+              ],
             ),
           ],
         ),
-      ],
-    ),
-    StatefulShellBranchData.$branch(
-      navigatorKey: SettingsBranch.$navigatorKey,
-      routes: [
-        GoRouteData.$route(
-          path: '/settings',
-          factory: $SettingsRouteExtension._fromState,
+        StatefulShellBranchData.$branch(
+          navigatorKey: SettingsBranch.$navigatorKey,
           routes: [
             GoRouteData.$route(
-              path: 'theme',
-              parentNavigatorKey: ThemeSettingDialogRoute.$parentNavigatorKey,
-              factory: $ThemeSettingDialogRouteExtension._fromState,
-            ),
-            GoRouteData.$route(
-              path: 'license',
-              parentNavigatorKey: LicenseRoute.$parentNavigatorKey,
-              factory: $LicenseRouteExtension._fromState,
+              path: '/settings',
+              factory: $SettingsRouteExtension._fromState,
+              routes: [
+                GoRouteData.$route(
+                  path: 'theme',
+                  parentNavigatorKey:
+                      ThemeSettingDialogRoute.$parentNavigatorKey,
+                  factory: $ThemeSettingDialogRouteExtension._fromState,
+                ),
+                GoRouteData.$route(
+                  path: 'license',
+                  parentNavigatorKey: LicenseRoute.$parentNavigatorKey,
+                  factory: $LicenseRouteExtension._fromState,
+                ),
+              ],
             ),
           ],
         ),
       ],
-    ),
-  ],
-);
+    );
 
 extension $AppShellRouteDataExtension on AppShellRouteData {
   static AppShellRouteData _fromState(GoRouterState state) =>
@@ -96,7 +98,9 @@ extension $AppShellRouteDataExtension on AppShellRouteData {
 extension $HomeRouteExtension on HomeRoute {
   static HomeRoute _fromState(GoRouterState state) => const HomeRoute();
 
-  String get location => GoRouteData.$location('/home');
+  String get location => GoRouteData.$location(
+        '/home',
+      );
 
   void go(BuildContext context) => context.go(location);
 
@@ -111,7 +115,9 @@ extension $HomeRouteExtension on HomeRoute {
 extension $FeedListRouteExtension on FeedListRoute {
   static FeedListRoute _fromState(GoRouterState state) => const FeedListRoute();
 
-  String get location => GoRouteData.$location('/home/feeds');
+  String get location => GoRouteData.$location(
+        '/home/feeds',
+      );
 
   void go(BuildContext context) => context.go(location);
 
@@ -124,11 +130,13 @@ extension $FeedListRouteExtension on FeedListRoute {
 }
 
 extension $FeedDetailRouteExtension on FeedDetailRoute {
-  static FeedDetailRoute _fromState(GoRouterState state) =>
-      FeedDetailRoute(feedId: state.pathParameters['feedId']!);
+  static FeedDetailRoute _fromState(GoRouterState state) => FeedDetailRoute(
+        feedId: state.pathParameters['feedId']!,
+      );
 
-  String get location =>
-      GoRouteData.$location('/home/feeds/${Uri.encodeComponent(feedId)}');
+  String get location => GoRouteData.$location(
+        '/home/feeds/${Uri.encodeComponent(feedId)}',
+      );
 
   void go(BuildContext context) => context.go(location);
 
@@ -144,7 +152,9 @@ extension $QuickAddQuestDialogRouteExtension on QuickAddQuestDialogRoute {
   static QuickAddQuestDialogRoute _fromState(GoRouterState state) =>
       const QuickAddQuestDialogRoute();
 
-  String get location => GoRouteData.$location('/home/quickAddQuest');
+  String get location => GoRouteData.$location(
+        '/home/quickAddQuest',
+      );
 
   void go(BuildContext context) => context.go(location);
 
@@ -160,7 +170,9 @@ extension $QuestListRouteExtension on QuestListRoute {
   static QuestListRoute _fromState(GoRouterState state) =>
       const QuestListRoute();
 
-  String get location => GoRouteData.$location('/quests');
+  String get location => GoRouteData.$location(
+        '/quests',
+      );
 
   void go(BuildContext context) => context.go(location);
 
@@ -175,7 +187,9 @@ extension $QuestListRouteExtension on QuestListRoute {
 extension $QuestAddRouteExtension on QuestAddRoute {
   static QuestAddRoute _fromState(GoRouterState state) => const QuestAddRoute();
 
-  String get location => GoRouteData.$location('/quests/add');
+  String get location => GoRouteData.$location(
+        '/quests/add',
+      );
 
   void go(BuildContext context) => context.go(location);
 
@@ -188,11 +202,13 @@ extension $QuestAddRouteExtension on QuestAddRoute {
 }
 
 extension $QuestDetailRouteExtension on QuestDetailRoute {
-  static QuestDetailRoute _fromState(GoRouterState state) =>
-      QuestDetailRoute(questId: state.pathParameters['questId']!);
+  static QuestDetailRoute _fromState(GoRouterState state) => QuestDetailRoute(
+        questId: state.pathParameters['questId']!,
+      );
 
-  String get location =>
-      GoRouteData.$location('/quests/${Uri.encodeComponent(questId)}');
+  String get location => GoRouteData.$location(
+        '/quests/${Uri.encodeComponent(questId)}',
+      );
 
   void go(BuildContext context) => context.go(location);
 
@@ -207,7 +223,9 @@ extension $QuestDetailRouteExtension on QuestDetailRoute {
 extension $SettingsRouteExtension on SettingsRoute {
   static SettingsRoute _fromState(GoRouterState state) => const SettingsRoute();
 
-  String get location => GoRouteData.$location('/settings');
+  String get location => GoRouteData.$location(
+        '/settings',
+      );
 
   void go(BuildContext context) => context.go(location);
 
@@ -223,7 +241,9 @@ extension $ThemeSettingDialogRouteExtension on ThemeSettingDialogRoute {
   static ThemeSettingDialogRoute _fromState(GoRouterState state) =>
       const ThemeSettingDialogRoute();
 
-  String get location => GoRouteData.$location('/settings/theme');
+  String get location => GoRouteData.$location(
+        '/settings/theme',
+      );
 
   void go(BuildContext context) => context.go(location);
 
@@ -238,7 +258,9 @@ extension $ThemeSettingDialogRouteExtension on ThemeSettingDialogRoute {
 extension $LicenseRouteExtension on LicenseRoute {
   static LicenseRoute _fromState(GoRouterState state) => const LicenseRoute();
 
-  String get location => GoRouteData.$location('/settings/license');
+  String get location => GoRouteData.$location(
+        '/settings/license',
+      );
 
   void go(BuildContext context) => context.go(location);
 
@@ -250,13 +272,17 @@ extension $LicenseRouteExtension on LicenseRoute {
   void replace(BuildContext context) => context.replace(location);
 }
 
-RouteBase get $authRoute =>
-    GoRouteData.$route(path: '/auth', factory: $AuthRouteExtension._fromState);
+RouteBase get $authRoute => GoRouteData.$route(
+      path: '/auth',
+      factory: $AuthRouteExtension._fromState,
+    );
 
 extension $AuthRouteExtension on AuthRoute {
   static AuthRoute _fromState(GoRouterState state) => const AuthRoute();
 
-  String get location => GoRouteData.$location('/auth');
+  String get location => GoRouteData.$location(
+        '/auth',
+      );
 
   void go(BuildContext context) => context.go(location);
 
@@ -269,15 +295,17 @@ extension $AuthRouteExtension on AuthRoute {
 }
 
 RouteBase get $debugScreenRoute => GoRouteData.$route(
-  path: '/debug',
-  factory: $DebugScreenRouteExtension._fromState,
-);
+      path: '/debug',
+      factory: $DebugScreenRouteExtension._fromState,
+    );
 
 extension $DebugScreenRouteExtension on DebugScreenRoute {
   static DebugScreenRoute _fromState(GoRouterState state) =>
       const DebugScreenRoute();
 
-  String get location => GoRouteData.$location('/debug');
+  String get location => GoRouteData.$location(
+        '/debug',
+      );
 
   void go(BuildContext context) => context.go(location);
 
@@ -290,16 +318,18 @@ extension $DebugScreenRouteExtension on DebugScreenRoute {
 }
 
 RouteBase get $dataStoreSettingsScreenRoute => GoRouteData.$route(
-  path: '/dataStoreSettings',
-  factory: $DataStoreSettingsScreenRouteExtension._fromState,
-);
+      path: '/dataStoreSettings',
+      factory: $DataStoreSettingsScreenRouteExtension._fromState,
+    );
 
 extension $DataStoreSettingsScreenRouteExtension
     on DataStoreSettingsScreenRoute {
   static DataStoreSettingsScreenRoute _fromState(GoRouterState state) =>
       const DataStoreSettingsScreenRoute();
 
-  String get location => GoRouteData.$location('/dataStoreSettings');
+  String get location => GoRouteData.$location(
+        '/dataStoreSettings',
+      );
 
   void go(BuildContext context) => context.go(location);
 
@@ -312,15 +342,17 @@ extension $DataStoreSettingsScreenRouteExtension
 }
 
 RouteBase get $onboardingRoute => GoRouteData.$route(
-  path: '/onboarding',
-  factory: $OnboardingRouteExtension._fromState,
-);
+      path: '/onboarding',
+      factory: $OnboardingRouteExtension._fromState,
+    );
 
 extension $OnboardingRouteExtension on OnboardingRoute {
   static OnboardingRoute _fromState(GoRouterState state) =>
       const OnboardingRoute();
 
-  String get location => GoRouteData.$location('/onboarding');
+  String get location => GoRouteData.$location(
+        '/onboarding',
+      );
 
   void go(BuildContext context) => context.go(location);
 
@@ -343,9 +375,8 @@ String _$routerHash() => r'1080abdf14ba8db6d25b56eb611d1f4ff955338b';
 final routerProvider = AutoDisposeProvider<GoRouter>.internal(
   router,
   name: r'routerProvider',
-  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
-      ? null
-      : _$routerHash,
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$routerHash,
   dependencies: null,
   allTransitiveDependencies: null,
 );
